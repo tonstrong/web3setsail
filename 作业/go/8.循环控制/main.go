6479bde5ce1e8fbe9d80213509c449372166858a
Alieon0 <yt16366@163.com>
2025-08-12 17:03:48 +0800

    go学习笔记

---
+++ 作业/go/8.循环控制/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	//声明方式1
+	for i := 0; i < 2; i++ {
+		fmt.Println(i)
+	}
+
+	//声明方式2
+	var a int = 3
+	for a > 2 {
+		fmt.Println(a)
+		a--
+	}
+
+	//声明方式3 无限循环
+	for {
+		fmt.Println("死循环")
+		break
+	}
+
+	//方式4
+	var arr [3]int = [3]int{1, 2, 3}
+	slice := arr[0:2]
+
+	var tmap map[string]string = make(map[string]string)
+	tmap["1"] = "1"
+	tmap["2"] = "2"
+	for i, v := range slice {
+		fmt.Println("slice[", i, "] = ", v)
+	}
+	for k, v := range tmap {
+		fmt.Println(k, v)
+	}
+
+	select {
+	case <-time.After(time.Second * 2):
+		fmt.Println("timeout")
+	case <-time.After(time.Second):
+		fmt.Println("过了2秒")
+		break
+	}
+
+}
